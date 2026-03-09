1e3d83b9d86edf03335a6f159689edddc85dff59
dcorp80 <10655954+dcorp80@users.noreply.github.com>
2026-03-09 13:19:11 +0100

    Tests now are part of CI

--- vitest.config.js
+++ vitest.config.js
@@ -1,17 +1,33 @@
 import { defineConfig } from 'vitest/config'
 
-const __REPORT_DIR__ = 'reports'
+const REPORT_DIR = 'reports'
 
 export default defineConfig({
     test: {
-        include: ['tests/headless/**/*.test.js', 'tests/unit/**/*.test.js'],
-        testTimeout: 30_000,
-        pool: 'forks',
-        fileParallelism: false,
-        reporters: ["default", "html"],
-        outputFile: `${__REPORT_DIR__}/index.html`,
+        reporters: ['default', 'blob'],
+        projects: [
+            {
+                test: {
+                    name: 'unit',
+                    include: ['tests/unit/**/*.test.js'],
+                },
+            },
+            {
+                define: {
+                    __REPORT_DIR__: JSON.stringify(REPORT_DIR),
+                },
+                test: {
+                    name: 'headless',
+                    include: ['tests/headless/**/*.test.js'],
+                    testTimeout: 30_000,
+                },
+            },
+        ],
+        coverage: {
+            provider: 'v8',
+            include: ['js/**'],
+            reporter: ['text', 'html', 'lcov'],
+            reportsDirectory: `${REPORT_DIR}/coverage`,
+        },
     },
-    define: {
-      __REPORT_DIR__: JSON.stringify(__REPORT_DIR__)
-    }
 })
